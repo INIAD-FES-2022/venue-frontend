fc4f7152208a6158f76109ed3b3faf1b41f7b27c
yu7400ki <yu7400ki@gmail.com>
2022-09-12 18:54:25 +0900

    fix useElementRect

--- src/hooks/useElementRect.ts
+++ src/hooks/useElementRect.ts
@@ -1,29 +1,32 @@
 import { useEffect, useState, useCallback } from 'react';
 
-export const useElementRect = (
-  ref: React.RefObject<HTMLElement>,
-): {
-  rect: DOMRect | undefined;
-  updateRect: () => void;
-} => {
+export const useElementRect = <T extends HTMLElement>(
+  ref: React.RefObject<T> | null,
+  updateTiming: string[],
+): DOMRect | undefined => {
   const [rect, setRect] = useState<DOMRect | undefined>(undefined);
 
   const updateRect = useCallback(() => {
-    if (ref.current) {
+    if (ref?.current) {
       setRect(ref.current.getBoundingClientRect());
     } else {
       setRect(undefined);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ref]);
 
   useEffect(() => {
     updateRect(); // initial call
+    updateTiming.forEach((event) => {
+      window.addEventListener(event, updateRect);
+    });
+
+    return () => {
+      updateTiming.forEach((event) => {
+        window.removeEventListener(event, updateRect);
+      });
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return {
-    rect,
-    updateRect,
-  };
+  return rect;
 };
