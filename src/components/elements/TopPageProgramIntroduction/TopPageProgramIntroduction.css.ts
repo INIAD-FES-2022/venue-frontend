8eceeaf1b4f9338bd54e588d0f191b3ba2a8f714
yu7400ki <yu7400ki@gmail.com>
2022-09-14 02:23:29 +0900

    improve loading display

--- src/components/elements/TopPageProgramIntroduction/TopPageProgramIntroduction.css.ts
+++ src/components/elements/TopPageProgramIntroduction/TopPageProgramIntroduction.css.ts
@@ -12,8 +12,17 @@ export const caption = style({
   fontSize: '2rem',
 });
 
+export const sliderWrapper = style({
+  'width': '90%',
+  '@media': {
+    'screen and (max-width: 600px)': {
+      width: '80%',
+    },
+  },
+});
+
 export const slider = style({
-  width: '90%',
+  width: '100%',
 });
 
 export const sliderItem = style({
@@ -26,7 +35,22 @@ export const thumbnail = style({
   objectFit: 'cover',
 });
 
+export const dummyImage = style({
+  width: '100%',
+  height: '100%',
+  aspectRatio: '16/9',
+  backgroundColor: '#CCCCCC',
+});
+
 export const title = style({
   textAlign: 'center',
   fontSize: '1.5rem',
+});
+
+export const dummyTitle = style({
+  width: '100%',
+  height: '2rem',
+  backgroundColor: '#CCCCCC',
+  transform: 'scale(0.6, 0.6)',
+  transformOrigin: 'center',
 });
--- src/components/elements/TopPageProgramIntroduction/TopPageProgramIntroduction.tsx
+++ src/components/elements/TopPageProgramIntroduction/TopPageProgramIntroduction.tsx
@@ -1,71 +1,95 @@
 import { useState, useEffect, useRef } from 'react';
-import Slider from 'react-slick';
+import Slider, { Settings } from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { useFetchAllPrograms } from '../../../hooks/useFetchAllPrograms';
 import { useElementRect } from '../../../hooks/useElementRect';
 import * as styles from './TopPageProgramIntroduction.css';
 
+const DummyItem = () => (
+  <div className={styles.sliderItem}>
+    <div className={styles.dummyImage}> </div>
+    <div className={styles.dummyTitle}> </div>
+  </div>
+);
+
+const sliderSettingInit = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  pauseOnHover: true,
+};
+
 export const TopPageProgramIntroduction = () => {
   const { data, isLoading, isError } = useFetchAllPrograms();
   const sliderRef = useRef(null);
   const rect = useElementRect<HTMLDivElement>(sliderRef, ['resize']);
-  const [sliderSetting, setSliderSetting] = useState({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    pauseOnHover: true,
-  });
+  const [sliderSetting, setSliderSetting] = useState<Settings | undefined>(
+    undefined,
+  );
 
   useEffect(() => {
-    if (rect?.width && data) {
-      const divNum = 300;
-      setSliderSetting((s) => {
-        const displayNum = Math.floor(rect.width / divNum);
-
-        return {
-          ...s,
-          slidesToShow: Math.min(displayNum, data.length),
-        };
-      });
+    if (rect?.width) {
+      const divNum = 250;
+      const displayNum = Math.floor(rect.width / divNum);
+      const setting = data
+        ? {
+            ...sliderSettingInit,
+            slidesToShow: Math.min(displayNum, data.length),
+          }
+        : {
+            ...sliderSettingInit,
+            slidesToShow: displayNum,
+          };
+      setSliderSetting(setting);
     }
   }, [rect?.width, data]);
 
   return (
-    <div className={styles.container} ref={sliderRef}>
+    <div className={styles.container}>
       <h2 className={styles.caption}>企画紹介</h2>
-      {(() => {
-        let result;
-        if (isLoading) {
-          result = <p>Now Loading...</p>;
-        } else if (isError) {
-          result = <p>Error</p>;
-        } else if (data) {
-          /* eslint-disable */
-          result = (
-            <Slider {...sliderSetting} className={styles.slider}>
-              {data.map((program) => {
-                return (
-                  <div key={program.uuid} className={styles.sliderItem}>
-                    <img
-                      src={program.thumbnail}
-                      alt={program.title}
-                      className={styles.thumbnail}
-                    />
-                    <h3 className={styles.title}>{program.title}</h3>
-                  </div>
-                );
-              })}
-            </Slider>
-          );
-          /* eslint-enable */
-        }
+      <div className={styles.sliderWrapper} ref={sliderRef}>
+        {(() => {
+          let result;
+          if (sliderSetting) {
+            if (isLoading) {
+              result = (
+                /* eslint-disable */
+                <Slider {...sliderSetting} className={styles.slider}>
+                  {[...Array(sliderSetting.slidesToShow).keys()].map((i) => (
+                    <DummyItem key={i} />
+                  ))}
+                </Slider>
+              );
+            } else if (isError) {
+              result = <p>Error</p>;
+            } else if (data) {
+              result = (
+                <Slider {...sliderSetting} className={styles.slider}>
+                  {data.map((program) => {
+                    return (
+                      <div key={program.uuid} className={styles.sliderItem}>
+                        <img
+                          src={program.thumbnail}
+                          alt={program.title}
+                          className={styles.thumbnail}
+                        />
+                        <h3 className={styles.title}>{program.title}</h3>
+                      </div>
+                    );
+                  })}
+                </Slider>
+              );
+              /* eslint-enable */
+            }
+          }
 
-        return result;
-      })()}
+          return result;
+        })()}
+      </div>
     </div>
   );
 };
