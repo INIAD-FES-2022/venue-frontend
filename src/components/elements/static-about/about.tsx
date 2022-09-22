59faa79c233ebad27146b66bad870ab0fbd126d5
1F10210099 <s1f102100995@iniad.org>
2022-09-23 04:12:02 +0900

    create static-about

---
+++ src/components/elements/static-about/about.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+export const about = () => {
+  <div>
+    <ul>
+      <li>
+        <Link href="/">
+          <h2>INIADについて</h2>
+          <p>xxxxxxxxx</p>
+        </Link>
+      </li>
+      <li>
+        <Link href="/">
+          <h2>パンフレット</h2>
+          <p>ダウンロード</p>
+        </Link>
+      </li>
+      <li>
+        <Link href="/">
+          <h2>テーマについて</h2>
+          <p>xxxxxxxxx</p>
+        </Link>
+      </li>
+      <li>
+        <Link href="/">
+          <h2> INIAD-FESについて</h2>
+          <p>xxxxxxxxx</p>
+        </Link>
+      </li>
+    </ul>
+  </div>;
+};
---
+++ src/components/elements/static-about/notes.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export const notes = () => {
+  <div>
+    <ul>
+      <li>
+        <Link href="/">
+          <h2>諸注意</h2>
+          <p>xxxxxxxxx</p>
+        </Link>
+      </li>
+    </ul>
+  </div>;
+};
