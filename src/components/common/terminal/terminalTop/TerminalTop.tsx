c2307f34ebc2a38e6705055021674cd3ca7ffca4
littleswampman <littleswampman@gmail.com>
2022-09-24 05:46:39 +0900

    develop terminalTop

--- src/components/common/terminal/terminalTop/TerminalTop.tsx
+++ src/components/common/terminal/terminalTop/TerminalTop.tsx
@@ -1,6 +1,77 @@
-import React from 'react';
+import { FC } from 'react';
 import { styles } from './terminalTop.css';
 
-export const TerminalTop = () => (
-  <div className={`${styles.container}`}>TerminalTop</div>
+type Props = {
+  size?: number;
+  onDeleteButtonClicked: () => void;
+  onMaximizeButtonClicked: () => void;
+  onMinimizeButtonClicked: () => void;
+  className?: string;
+  isMinimized?: boolean;
+};
+
+export const TerminalTop: FC<Props> = ({
+  size = 24,
+  onDeleteButtonClicked,
+  onMaximizeButtonClicked,
+  onMinimizeButtonClicked,
+  className = '',
+  isMinimized = false,
+}) => (
+  <div
+    className={`${className} ${styles.container} ${
+      isMinimized ? 'rounded-md' : 'rounded-t-md'
+    } flex h-6 w-full flex-row-reverse items-center`}
+  >
+    <button
+      type="button"
+      className={`${styles.deleteButton} flex h-full w-8 items-center justify-center`}
+      onClick={onDeleteButtonClicked}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="none"
+          stroke="#FFF"
+          strokeWidth={2}
+          d="M3,3 L21,21 M3,21 L21,3"
+        />
+      </svg>
+    </button>
+    <button
+      type="button"
+      className={`${styles.maximizeButton} flex h-full w-8 items-center justify-center`}
+      onClick={onMaximizeButtonClicked}
+    >
+      <svg
+        width="24"
+        height="16"
+        viewBox="0 0 24 16"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g id="layer1">
+          <rect fill="#ffffff" width="24" height="16" x="0" y="0" />
+          <rect fill="#000000" width="22" height="14" x="1" y="1" />
+        </g>
+      </svg>
+    </button>
+    <button
+      type="button"
+      className={`${styles.maximizeButton} flex h-full w-8 flex-col-reverse items-center`}
+      onClick={onMinimizeButtonClicked}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <line x1={4} y1={14} x2={20} y2={14} stroke="#FFF" strokeWidth={2} />
+      </svg>
+    </button>
+  </div>
 );
--- src/components/common/terminal/terminalTop/terminalTop.css.ts
+++ src/components/common/terminal/terminalTop/terminalTop.css.ts
@@ -1,7 +1,38 @@
-import { style } from '@vanilla-extract/css';
+import { style, createVar } from '@vanilla-extract/css';
+import { terminalVars } from '../terminalTheme.css';
+
+export const size = createVar();
 
 export const styles = {
   container: style({
-    backgroundColor: 'gray',
+    backgroundColor: terminalVars.colors.terminalTop_background_lightBrown,
+  }),
+  deleteButton: style({
+    'backgroundColor': terminalVars.colors.terminalTop_black,
+    'borderTopRightRadius': '0.375rem',
+    ':hover': {
+      backgroundColor: terminalVars.colors.terminalTop_red,
+    },
+    ':active': {
+      backgroundColor: terminalVars.colors.terminalTop_red,
+    },
+  }),
+  maximizeButton: style({
+    'backgroundColor': terminalVars.colors.terminalTop_black,
+    ':hover': {
+      backgroundColor: terminalVars.colors.terminalTop_black_lighter,
+    },
+    ':active': {
+      backgroundColor: terminalVars.colors.terminalTop_black_darker,
+    },
+  }),
+  minimizeButton: style({
+    'backgroundColor': terminalVars.colors.terminalTop_black,
+    ':hover': {
+      backgroundColor: terminalVars.colors.terminalTop_black_lighter,
+    },
+    ':active': {
+      backgroundColor: terminalVars.colors.terminalTop_black_darker,
+    },
   }),
 };
