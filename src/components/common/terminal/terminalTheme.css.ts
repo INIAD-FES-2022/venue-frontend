6dedf43775abf8d7bd3f0adb72ecf5223a92dc55
littleswampman <littleswampman@gmail.com>
2022-09-24 04:19:46 +0900

    add colors to terminalTheme

--- src/components/common/terminal/terminalTheme.css.ts
+++ src/components/common/terminal/terminalTheme.css.ts
@@ -2,6 +2,15 @@ import { createTheme } from '@vanilla-extract/css';
 import { colors, fonts } from '../../../styles/themes.css';
 
 export const [terminalThemeClass, terminalVars] = createTheme({
-  colors: { ...colors, background: '#240A0A', text: '#fafafa' },
+  colors: {
+    ...colors,
+    terminal_background_brown: '#240A0A',
+    terminal_text_white: '#fafafa',
+    terminal_white: '#fafafa',
+    terminalTop_background_lightBrown: '#5f4c4c',
+    terminalTop_text_white: '#fafafa',
+    terminalTop_text_black: '#24292f',
+    terminalTop_red: '#e81123',
+  },
   fonts: { ...fonts },
 });
