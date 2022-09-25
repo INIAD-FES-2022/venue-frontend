45c5308dc500409069be443e8e894385c3d78658
littleswampman <littleswampman@gmail.com>
2022-09-25 16:02:53 +0900

    fix svg

--- src/components/common/terminal/terminalTop/TerminalTop.tsx
+++ src/components/common/terminal/terminalTop/TerminalTop.tsx
@@ -43,7 +43,7 @@ export const TerminalTop: FC<Props> = ({
         onClick={onDeleteButtonClicked}
       >
         <path
-          stroke="#fff"
+          stroke="#EFEFEF"
           strokeWidth={2}
           d={`M 0.7071067811865475,0.7071067811865475 ${
             barHeight * (2 / 3) - 0.7071067811865475
@@ -52,7 +52,7 @@ export const TerminalTop: FC<Props> = ({
           // 1 / √2 * (2※strokeWidth / 2) = 0.7071067811865475
         />
         <path
-          stroke="#fff"
+          stroke="#EFEFEF"
           strokeWidth={2}
           d={`M ${
             barHeight * (2 / 3) - 0.7071067811865475
@@ -74,26 +74,21 @@ export const TerminalTop: FC<Props> = ({
     >
       <svg
         width={barHeight}
-        height={barHeight}
+        height={barHeight * (2 / 3)}
         viewBox={`0 0 ${barHeight} ${barHeight * (2 / 3)}`}
         // "0 0 24 16"
         xmlns="http://www.w3.org/2000/svg"
         onClick={onMaximizeButtonClicked}
       >
-        <rect
-          fill="#ffffff"
-          width={barHeight}
-          height={barHeight * (2 / 3)}
-          x="0"
-          y="0"
+        <path
+          d={`M0 0v${barHeight * (2 / 3)}h${barHeight}V0zm1 1h${
+            barHeight - 2
+          }v${barHeight * (2 / 3) - 2}H1z`}
+          fill="#EFEFEF"
         />
-        <rect
-          fill="#000000"
-          width={barHeight - 2}
-          height={barHeight * (2 / 3) - 2}
-          x="1"
-          y="1"
-        />
+        {/* <svg width="24" height="16" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 0v16h24V0zm1 1h22v14H1z" fill="#EFEFEF" />
+        </svg> */}
       </svg>
     </button>
     <button
@@ -106,17 +101,17 @@ export const TerminalTop: FC<Props> = ({
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
+        width={barHeight * 0.7}
+        height={barHeight}
+        viewBox={`0 0 ${barHeight} ${barHeight}`}
         onClick={onMinimizeButtonClicked}
       >
         <line
-          x1={4}
-          y1={barHeight * (2 / 3) - 2}
-          x2={barHeight - 4}
-          y2={barHeight * (2 / 3) - 2}
-          stroke="#FFF"
+          x1={0}
+          y1={barHeight * 0.8}
+          x2={barHeight}
+          y2={barHeight * 0.8}
+          stroke="#EFEFEF"
           strokeWidth={2}
         />
       </svg>
