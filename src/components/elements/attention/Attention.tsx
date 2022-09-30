7c2f185c72aff0c4f3b332097c18974bf9fe9a66
1F10210099 <s1f102100995@iniad.org>
2022-10-01 04:06:20 +0900

    create Attention.tsx css

---
+++ src/components/elements/attention/Attention.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import * as styles from './Attention.css';
+
+export const Attention: React.FC = () => (
+  <div className={styles.attention}>
+    <h1 className={styles.title}>諸注意</h1>
+    <p className={styles.caption}>ダミーーーー</p>
+  </div>
+);
