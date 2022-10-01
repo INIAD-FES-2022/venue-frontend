8c4a119633fb8fe46b782d7301f6c3e371df8a24
Yoshiki333 <s1F102000286@iniad.org>
2022-10-01 10:33:25 +0900

    create access

---
+++ src/components/elements/StaticComponents/Access.css.ts
@@ -0,0 +1,11 @@
+import { style } from '@vanilla-extract/css';
+
+export const wrapper = style({
+  margin: 'auto',
+  maxWidth: '800px',
+  fontSize: '1rem',
+});
+export const sectionTitle = style({
+  fontSize: '1.2rem',
+  textAlign: 'left',
+});
---
+++ src/components/elements/StaticComponents/Access.tsx
@@ -0,0 +1,9 @@
+import { wrapper, sectionTitle } from './Access.css';
+
+export const Access: React.FC = () => (
+  <div className={wrapper}>
+    <h2 className={sectionTitle}>アクセス</h2>
+    <br />
+    <p>〒115-8650 東京都北区赤羽台1-7-11</p>
+  </div>
+);
