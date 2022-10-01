ed3aa6809332cdae20ab3225687f321ca53ab213
1F10210099 <s1f102100995@iniad.org>
2022-10-01 17:47:03 +0900

    change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Sample } from '../components/elements/Sample/Sample';
 import { Sample2 } from '../components/elements/Sample2/Sample2';
+import { Committee } from '../components/elements/Committee/Committee';
 
 const Home: NextPage = () => (
   <div className="flex min-h-screen flex-col items-center justify-center py-2">
@@ -20,6 +21,7 @@ const Home: NextPage = () => (
       </h1>
       <Sample />
       <Sample2 />
+      <Committee />
 
       <p className="mt-3 text-2xl">
         Get started by editing{' '}
