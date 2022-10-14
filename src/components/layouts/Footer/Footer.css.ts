1ae5305848df9cf001934a910e4262ff6903d837
yu7400ki <yu7400ki@gmail.com>
2022-10-15 02:09:38 +0900

    create footer component

--- src/components/layouts/Footer/Footer.css.ts
+++ src/components/layouts/Footer/Footer.css.ts
@@ -1,9 +1,46 @@
 import { style } from '@vanilla-extract/css';
-import { globalVars } from '../../../styles/themes.css';
+import { fonts } from '../../../styles/themes.css';
 
-export const styles = {
-  container: style({
-    backgroundColor: globalVars.colors.sub_yellow_1,
-    borderRadius: '0.375rem',
-  }),
-};
+export const footer = style({
+  display: 'grid',
+  gridTemplateColumns: '18rem 1fr',
+  fontFamily: `${fonts.japanese_1}, ${fonts.japanese_2}, ${fonts.japanese_3}, sans-serif`,
+  fontSize: '1.5rem',
+  gap: '0.5rem',
+  padding: '1rem',
+});
+
+export const title = style({
+  gridRow: '1 / 2',
+  gridColumn: '1 / 3',
+  fontSize: '2rem',
+  marginBottom: '1rem',
+});
+
+export const headline3 = style({
+  display: 'flex',
+  justifyContent: 'center',
+});
+
+export const sns = style({
+  gridColumn: '1 / 3',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexWrap: 'wrap',
+  gap: '5rem',
+  rowGap: '1rem',
+  marginTop: '1rem',
+});
+
+export const snsLink = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '1rem',
+});
+
+export const snsText = style({
+  fontSize: '1.5rem',
+  paddingBottom: '0.2rem',
+});
--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -1,3 +1,42 @@
 import React from 'react';
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
 
-export const Footer: React.FC = () => <div>Footer</div>;
+import { Terminal } from '../../common/terminal/Terminal';
+import * as styles from './Footer.css';
+
+type Props = {
+  className?: string;
+};
+
+export const Footer: React.FC<Props> = ({ className }) => (
+  <Terminal barTitle="脚注" className={className}>
+    <div className={styles.footer}>
+      <h2 className={styles.title}>関連さいと</h2>
+      <h3 className={styles.headline3}>東洋大学公式さいと</h3>
+      <ul>
+        <li>東洋大学</li>
+        <li>INIAD東洋大学情報連携学部</li>
+      </ul>
+      <h3 className={styles.headline3}>他きゃんぱす大学祭</h3>
+      <ul>
+        <li>東洋大学白山キャンパス大学祭 第56回 白山祭</li>
+        <li>東洋大学ライフデザイン学部大学祭 第2回 WELLB-FES</li>
+        <li>東洋大学板倉キャンパス大学祭 第24回 雷祭</li>
+        <li>東洋大学川越キャンパス大学祭 こもれび祭</li>
+      </ul>
+      <div className={styles.sns}>
+        <a href="https://twitter.com/iniadfes" className={styles.snsLink}>
+          <span className={styles.snsText}>Twitter</span>
+          <BsTwitter size={40} />
+        </a>
+        <a
+          href="https://www.instagram.com/iniad_fes/"
+          className={styles.snsLink}
+        >
+          <span className={styles.snsText}>Instagram</span>
+          <BsInstagram size={40} />
+        </a>
+      </div>
+    </div>
+  </Terminal>
+);
