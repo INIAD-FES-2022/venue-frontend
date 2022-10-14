879afbb0435dab16a950c6901515e62f0c62b114
yu7400ki <yu7400ki@gmail.com>
2022-10-15 02:10:28 +0900

    cretae layout for pc

--- src/components/layouts/Layout.css.ts
+++ src/components/layouts/Layout.css.ts
@@ -1,9 +1,20 @@
 import { style } from '@vanilla-extract/css';
-import { globalVars } from '../../styles/themes.css';
 
-export const styles = {
-  container: style({
-    backgroundColor: globalVars.colors.sub_yellow_1,
-    borderRadius: '0.375rem',
-  }),
-};
+export const main = style({
+  display: 'grid',
+  alignItems: 'start',
+  gridTemplateColumns: '1fr minmax(20%, 16rem)',
+  gap: '1.5rem',
+  padding: '1.5rem',
+});
+
+export const nav = style({
+  gridColumn: '-2 / -1',
+  gridRow: '1 / 3',
+  position: 'sticky',
+  top: '1.5rem',
+});
+
+export const content = style({
+  minHeight: '95vh',
+});
--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,21 +1,21 @@
-import { FC, ReactNode, useState } from 'react';
-import { styles } from './Layout.css';
+import { FC, ReactNode } from 'react';
 
+import * as styles from './Layout.css';
 import { Nav } from './Nav/Nav';
 import { Footer } from './Footer/Footer';
+import { Terminal } from '../common/terminal/Terminal';
 
 type Props = {
   children: ReactNode;
+  title: string;
 };
 
-export const Layout: FC<Props> = ({ children }) => {
-  const layout = true;
-
-  return (
-    <>
-      <Nav />
-      <main>{children}</main>
-      <Footer />
-    </>
-  );
-};
+export const Layout: FC<Props> = ({ children, title }) => (
+  <div className={styles.main}>
+    <Terminal barTitle={title} className={styles.content}>
+      {children}
+    </Terminal>
+    <Nav className={styles.nav} />
+    <Footer />
+  </div>
+);
