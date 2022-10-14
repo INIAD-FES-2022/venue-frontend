82e7bc782f010ae19eede4849e0debb1a6ec9004
yu7400ki <yu7400ki@gmail.com>
2022-10-15 00:28:43 +0900

    create navigation component

--- src/components/layouts/Nav/Nav.css.ts
+++ src/components/layouts/Nav/Nav.css.ts
@@ -1,9 +1,13 @@
 import { style } from '@vanilla-extract/css';
-import { globalVars } from '../../../styles/themes.css';
+import { fonts } from '../../../styles/themes.css';
 
-export const styles = {
-  container: style({
-    backgroundColor: globalVars.colors.sub_yellow_1,
-    borderRadius: '0.375rem',
-  }),
-};
+export const nav = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  gap: '0.5rem',
+  padding: '0.5rem',
+  fontSize: '1.5rem',
+  fontFamily: `${fonts.japanese_1}, ${fonts.japanese_2}, ${fonts.japanese_3}, sans-serif`,
+});
--- src/components/layouts/Nav/Nav.tsx
+++ src/components/layouts/Nav/Nav.tsx
@@ -1,3 +1,33 @@
 import React from 'react';
+import Link from 'next/link';
 
-export const Nav: React.FC = () => <div>Header</div>;
+import { Terminal } from '../../common/terminal/Terminal';
+import * as styles from './Nav.css';
+
+export const Nav: React.FC = () => (
+  <Terminal barTitle="誘導">
+    <ul className={styles.nav}>
+      <li>
+        <Link href="./">入口</Link>
+      </li>
+      <li>
+        <Link href="./">紹介</Link>
+      </li>
+      <li>
+        <Link href="./program">企画一覧</Link>
+      </li>
+      <li>
+        <Link href="./attention">諸注意</Link>
+      </li>
+      <li>
+        <Link href="./access">来場方法</Link>
+      </li>
+      <li>
+        <Link href="./schedule">日程表</Link>
+      </li>
+      <li>
+        <Link href="./contact">問い合わせ</Link>
+      </li>
+    </ul>
+  </Terminal>
+);
