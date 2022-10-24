0c9e80d6296408fa409c279db818e4efc2b2e9e4
1F10210099 <s1f102100995@iniad.org>
2022-10-25 03:13:07 +0900

    update css

--- src/components/elements/announcement/Toppageannnouncement.tsx
+++ src/components/elements/announcement/Toppageannnouncement.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useFetchAllNotices } from '../../../hooks/api/useFetchAllNotices';
 import * as styles from './TopPageannouncement.css';
 
-export const Notice = () => {
+export const Notice: React.FC = () => {
   const { data } = useFetchAllNotices({
     'sort-by': 'date',
   });
@@ -10,11 +10,13 @@ export const Notice = () => {
   return (
     <div className={styles.container}>
       <h2 className={styles.caption}>お知らせ</h2>
-      {data?.map((notice) => (
-        <div key={notice.uuid} className={styles.content}>
-          <h2 className={styles.title}>{notice.title}</h2>
-        </div>
-      ))}
+      <div className={styles.list}>
+        {data?.map((notice) => (
+          <div key={notice.uuid} className={styles.content}>
+            <p className={styles.title}>{notice.title}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
