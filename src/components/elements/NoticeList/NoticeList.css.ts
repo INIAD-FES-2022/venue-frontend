7255325fbf0e448bdd124cb5218a8436cc4d66f2
yu7400ki <yu7400ki@gmail.com>
2022-10-24 23:34:01 +0900

    fix notice list styles

--- src/components/elements/NoticeList/NoticeList.css.ts
+++ src/components/elements/NoticeList/NoticeList.css.ts
@@ -2,17 +2,23 @@ import { style, globalStyle } from '@vanilla-extract/css';
 
 export const container = style({
   width: '100%',
+  maxWidth: '1000px',
   height: '100%',
+  margin: '30px auto',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
-  backgroundColor: '#FFF5EA',
+  backgroundColor: '#FEF5EA',
   gap: '20px',
-  padding: '20px',
+  padding: '30px',
+  borderRadius: '50px',
+  boxShadow:
+    '0 4px 4px 0 rgba(0, 0, 0, 0.25), inset 0 4px 4px 0 rgba(0, 0, 0, 0.25)',
 });
 
 globalStyle(`${container} > *`, {
+  width: '100%',
   maxWidth: '1000px',
   padding: '0 20px 0 20px',
 });
@@ -38,48 +44,6 @@ export const title = style({
 
 export const noticeItem = style({
   width: '100%',
-  padding: '20px 0 20px 0',
+  paddingBottom: '20px',
   borderBottom: '1px solid #e0e0e0',
-});
-
-export const noticeContent = style({
-  width: '100%',
-  padding: '20px 0 20px 0',
-  fontSize: '1.2rem',
-});
-
-export const withIcon = style({
-  display: 'flex',
-  alignItems: 'center',
-  gap: '0.2rem',
-});
-
-export const AccordionWrapper = style({
-  width: '100%',
-});
-
-export const AccordionStyle = style({
-  width: '100%',
-});
-
-export const closeStyle = style({
-  height: '0',
-  overflow: 'hidden',
-});
-
-export const openArrow = style({
-  transform: 'ScaleY(-1)',
-  margin: '0 auto',
-  transition: 'transform 0.3s',
-});
-
-export const closeArrow = style({
-  transform: 'ScaleY(1)',
-});
-
-export const accordionToggle = style({
-  width: '100%',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
 });
