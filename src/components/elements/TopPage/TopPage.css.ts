5d8ecd4ec97ef9547c4bc90ec3ffa6330ed724de
littleswampman <littleswampman@gmail.com>
2022-10-25 01:15:27 +0900

    create TopPage.tsx & TopPage.css.ts

---
+++ src/components/elements/TopPage/TopPage.css.ts
@@ -0,0 +1,17 @@
+import { style } from '@vanilla-extract/css';
+
+const smartPhone = 'screen and (max-width: 52rem)';
+
+export const main = style({});
+
+export const nav = style({});
+
+export const footer = style({});
+
+export const mainVisual = style({});
+
+export const programIntroduction = style({});
+
+export const announcement = style({});
+
+export const sponsored = style({});
---
+++ src/components/elements/TopPage/TopPage.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Nav } from '../../layouts/Nav/Nav';
+import { Footer } from '../../layouts/Footer/Footer';
+import { Terminal } from '../../common/terminal/Terminal';
+import * as layoutStyle from '../../layouts/Layout.css';
+import * as styles from './TopPage.css';
+
+export const TopPage = () => (
+  <>
+    <Nav className={`${styles.nav}`} />
+    <main className={`${styles.main}`}>
+      <Terminal barTitle="看板" className={`${styles.mainVisual}`}>
+        <p>sample text</p>
+      </Terminal>
+      <Terminal barTitle="企画紹介" className={`${styles.programIntroduction}`}>
+        <p>sample text</p>
+      </Terminal>
+      <Terminal barTitle="お知らせ" className={`${styles.announcement}`}>
+        <p>sample text</p>
+      </Terminal>
+      <Terminal barTitle="協賛" className={`${styles.sponsored}`}>
+        <p>sample text</p>
+      </Terminal>
+    </main>
+    <Footer className={`${styles.footer}`} />
+  </>
+);
