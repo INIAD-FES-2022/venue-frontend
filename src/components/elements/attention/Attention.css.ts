eae15318484d9ebe9beaffb5c3d7ab348257bba1
1F10210099 <s1f102100995@iniad.org>
2022-10-26 01:22:08 +0900

    add png

--- src/components/elements/attention/Attention.css.ts
+++ src/components/elements/attention/Attention.css.ts
@@ -1,9 +1,16 @@
 import { style } from '@vanilla-extract/css';
 
 export const attention = style({
-  backgroundColor: 'fff1cf',
+  display: 'flex',
 });
 
 export const title = style({});
 
 export const caption = style({});
+
+export const img = style({});
+
+export const text = style({
+  display: 'inline-block',
+  verticalAlign: 'text-top',
+});
--- src/components/elements/attention/Attention.tsx
+++ src/components/elements/attention/Attention.tsx
@@ -4,10 +4,29 @@ import * as styles from './Attention.css';
 export const Attention: React.FC = () => (
   <div className={styles.attention}>
     <h1 className={styles.title}>諸注意</h1>
-    <p className={styles.caption}>
-      お車でのご来場はできません。公共交通機関をご利用ください。
-      大学祭に関するお問い合わせは、INIAD-FES実行委員会の公式SNSまたは電子メールまでお願いします。東洋大学ならびに情報連携学部、ライフデザイン学部への電話、電子メールでのお問い合わせはご遠慮ください。
-      感染症対策にご理解とご協力をお願いします。
-    </p>
+    <div className={styles.caption}>
+      <figure>
+        <img src="./car1.png" />
+      </figure>
+      <p className={styles.text}>お車でのご来場はできません。</p>
+      <p className={styles.text}>公共交通機関をご利用ください。</p>
+
+      <figure>
+        <img src="./info.png" />
+      </figure>
+      <span className={styles.text}>
+        大学祭に関するお問い合わせは、INIAD-FES実行委員会の公式SNSまたは電子メールまでお願いします。
+      </span>
+      <span className={styles.text}>
+        東洋大学ならびに情報連携学部、ライフデザイン学部への電話、電子メールでのお問い合わせはご遠慮ください。
+      </span>
+
+      <figure>
+        <img src="./covid.png" />
+      </figure>
+      <span className={styles.text}>
+        感染症対策にご理解とご協力をお願いします。
+      </span>
+    </div>
   </div>
 );
