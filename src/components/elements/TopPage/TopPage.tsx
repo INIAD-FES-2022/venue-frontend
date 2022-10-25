6620c4f80c219083bb393558c99f4bb0e6fb7cfa
littleswampman <littleswampman@gmail.com>
2022-10-25 16:58:27 +0900

    import components to TopPage.tsx

--- src/components/elements/TopPage/TopPage.tsx
+++ src/components/elements/TopPage/TopPage.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { Nav } from '../../layouts/Nav/Nav';
 import { Footer } from '../../layouts/Footer/Footer';
 import { Terminal } from '../../common/terminal/Terminal';
-import * as layoutStyle from '../../layouts/Layout.css';
+
+import { MainVisual } from './MainVIsual/MainVisual';
+import { TopPageProgramIntroduction } from './TopPageProgramIntroduction/TopPageProgramIntroduction';
+import { TopPageAnnouncement } from './TopPageAnnouncement/TopPageAnnouncement';
+import { Sponsored } from './Sponsored/Sponsored';
+
 import * as styles from './TopPage.css';
 
 export const TopPage = () => (
@@ -10,16 +15,16 @@ export const TopPage = () => (
     <Nav className={`${styles.nav}`} />
     <main className={`${styles.main} `}>
       <Terminal barTitle="看板" className={`${styles.mainVisual}`}>
-        <p>sample text</p>
+        <MainVisual />
       </Terminal>
       <Terminal barTitle="企画紹介" className={`${styles.programIntroduction}`}>
-        <p>sample text</p>
+        <TopPageProgramIntroduction />
       </Terminal>
       <Terminal barTitle="お知らせ" className={`${styles.announcement}`}>
-        <p>sample text</p>
+        <TopPageAnnouncement />
       </Terminal>
       <Terminal barTitle="協賛" className={`${styles.sponsored}`}>
-        <p>sample text</p>
+        <Sponsored />
       </Terminal>
     </main>
     <Footer className={`${styles.footer}`} />
