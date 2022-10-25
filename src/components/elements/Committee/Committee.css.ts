32a30fab3125def6422855ffaaf7e8a2bb732f94
1F10210099 <s1f102100995@iniad.org>
2022-10-26 08:43:34 +0900

    update tsx

--- src/components/elements/Committee/Committee.css.ts
+++ src/components/elements/Committee/Committee.css.ts
@@ -4,8 +4,19 @@ export const committee = style({
   backgroundColor: 'fff1cf',
 });
 
-export const greeting = style({});
-
+export const title = style({});
 export const caption = style({});
-
 export const roster = style({});
+
+export const container = style({
+  display: '-webkit-flex',
+  WebkitFlexDirection: 'column',
+  flexDirection: 'column',
+  WebkitFlexWrap: 'wrap',
+  flexWrap: 'wrap',
+  WebkitAlignSelf: 'center',
+  alignSelf: 'center',
+  height: '500px',
+  gap: '7px',
+  alignContent: 'space-between',
+});
