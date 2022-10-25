a068a16c63dc4dffa0f43aec2241ece2ed8f7f76
1F10210099 <s1f102100995@iniad.org>
2022-10-25 23:37:00 +0900

    update about.css

--- src/components/elements/About/About.css.ts
+++ src/components/elements/About/About.css.ts
@@ -1,7 +1,46 @@
 import { style } from '@vanilla-extract/css';
 
-export const container = style({});
+export const container = style({
+  width: '100%',
+  maxWidth: '1000px',
+  height: '100%',
+  margin: '30px auto',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: '#FEF5EA',
+  gap: '20px',
+  padding: '30px',
+});
 
-export const title = style({});
+export const siteTtl = style({
+  background: '#811C1F',
+  padding: '5px',
+  color: '#FFF',
+  fontFamily: 'bold',
+  letterSpacing: '0.05em',
+});
+export const title = style({
+  color: '#811C1F',
+  textAlign: 'left',
+  fontSize: '21px',
+});
 
-export const caption = style({});
+export const caption = style({
+  width: '100%',
+  textAlign: 'left',
+});
+
+export const univ = style({});
+
+export const FES = style({});
+
+export const theme = style({});
+
+export const img = style({
+  width: '50px',
+  textAlign: 'center',
+  minHeight: '0%',
+  display: 'inline-block',
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Sample } from '../components/elements/Sample/Sample';
 import { Sample2 } from '../components/elements/Sample2/Sample2';
+import { Caption } from '../components/elements/About/About';
 
 const Home: NextPage = () => (
   <div className="flex min-h-screen flex-col items-center justify-center py-2">
@@ -20,7 +21,7 @@ const Home: NextPage = () => (
       </h1>
       <Sample />
       <Sample2 />
-
+      <Caption />
       <p className="mt-3 text-2xl">
         Get started by editing{' '}
         <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
