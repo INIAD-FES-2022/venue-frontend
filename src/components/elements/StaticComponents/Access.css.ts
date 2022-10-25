214c7927252d832c21b4ca26079e1695fa0488fd
Yoshiki333 <s1F102000286@iniad.org>
2022-10-26 07:50:44 +0900

    アクセスとお問い合わせの大枠

--- src/components/elements/StaticComponents/Access.css.ts
+++ src/components/elements/StaticComponents/Access.css.ts
@@ -3,13 +3,34 @@ import { style } from '@vanilla-extract/css';
 export const accessStyle = {
   wrapper: style({
     margin: 'auto',
+    marginTop: '50px',
+    borderStyle: 'solid',
+    borderColor: '#F00',
+    borderWidth: '10px',
     width: '800px',
     fontSize: '1rem',
     textAlign: 'left',
-    padding: '40px',
   }),
 
   sectionTitle: style({
     fontSize: '1.5rem',
+    color: '#fff',
+    backgroundColor: '#F00',
+    width: '100%',
+    padding: '20px',
+  }),
+
+  contents: style({
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    color: '#000',
+    padding: '50px',
+  }),
+
+  img: style({
+    height: '800px',
+    width: '800px',
+    padding: '20px',
   }),
 };
+
+/*  */
--- src/components/elements/StaticComponents/Access.tsx
+++ src/components/elements/StaticComponents/Access.tsx
@@ -3,11 +3,16 @@ import { accessStyle } from './Access.css';
 export const Access: React.FC = () => (
   <div className={accessStyle.wrapper}>
     <h2 className={accessStyle.sectionTitle}>アクセス</h2>
-    <br />
-    <p>〒115-8650</p>
-    <p>東京都北区赤羽台1-7-11 INIAD HUB-1</p>
-    <br />
-    <p>※お車での来場はできません。公共交通機関をご利用ください。</p>
-    <img />
+    <div className={accessStyle.contents}>
+      <p>〒115-8650</p>
+      <p>東京都北区赤羽台1-7-11 INIAD HUB-1</p>
+      <br />
+      <p>※お車での来場はできません。公共交通機関をご利用ください。</p>
+      <img
+        className={accessStyle.img}
+        src="./map.png"
+        alt="赤羽駅西口から徒歩10分"
+      />
+    </div>
   </div>
 );
