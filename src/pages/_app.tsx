8c0f2ea7b1e2226eda6ce938c419c868476b88d7
yu7400ki <yu7400ki@gmail.com>
2022-10-26 01:28:39 +0900

    add query client & layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,34 @@
-import { NextComponentType, NextPageContext } from 'next';
-import { ReactNode, FC } from 'react';
+import { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import { ReactElement, ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import '../styles/globals.css';
 
-type Props<P = Record<string, unknown>> = {
-  Component: NextComponentType<NextPageContext, unknown, P>;
-  pageProps: ReactNode;
+const queryClient = new QueryClient();
+
+export type NextPageWithLayout<P = Record<never, never>, IP = P> = NextPage<
+  P,
+  IP
+> & {
+  getLayout?: (page: ReactElement) => ReactNode;
 };
 
-const MyApp: FC<Props> = ({ Component, pageProps }) => (
-  <Component>{pageProps}</Component>
-);
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  /* eslint-disable react/jsx-props-no-spreading */
+  return getLayout(
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>,
+  );
+};
 
 // eslint-disable-next-line import/no-default-export
 export default MyApp;
