becebb8417f3aba775ad4e0c14528967fa055c00
littleswampman <littleswampman@gmail.com>
2022-10-25 15:42:54 +0900

    fix the code to refer to the code instead of copying and pasting

--- src/components/elements/TopPage/TopPage.css.ts
+++ src/components/elements/TopPage/TopPage.css.ts
@@ -1,40 +1,13 @@
 import { style } from '@vanilla-extract/css';
+import * as layoutStyle from '../../layouts/Layout.css';
 
 const smartPhone = 'screen and (max-width: 52rem)';
 
-export const container = style({
-  'display': 'grid',
-  'alignItems': 'start',
-  'gridTemplateColumns': '1fr minmax(18%, 16rem)',
-  'gap': '1.5rem',
-  'padding': '1.5rem',
+export const container = style([layoutStyle.main]);
 
-  '@media': {
-    [smartPhone]: {
-      gridTemplateColumns: '1fr',
-    },
-  },
-});
+export const main = style([layoutStyle.content]);
 
-export const main = style({
-  minHeight: '95vh',
-});
-
-export const nav = style({
-  'gridColumn': '-2 / -1',
-  'gridRow': '1 / 3',
-  'position': 'sticky',
-  'top': '1.5rem',
-
-  '@media': {
-    [smartPhone]: {
-      margin: '0 auto',
-      width: '80%',
-      gridColumn: '1 / 2',
-      gridRow: '1 / 2',
-    },
-  },
-});
+export const nav = style([layoutStyle.nav]);
 
 export const footer = style({});
 
