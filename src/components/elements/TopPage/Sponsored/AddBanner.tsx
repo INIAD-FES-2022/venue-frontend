304e51dee89af24174bd4b189855ba7df24143a0
littleswampman <littleswampman@gmail.com>
2022-10-26 21:37:12 +0900

    apply design to sponsored

--- src/components/elements/TopPage/Sponsored/AddBanner.tsx
+++ src/components/elements/TopPage/Sponsored/AddBanner.tsx
@@ -1,7 +1,7 @@
 import { sponsoredStyle } from './Sponsored.css';
 
 type BannerItem = {
-  name: string; // 名前
+  // name: string; // 名前
   url: string; // URL
   img: string; // バナー画像
 };
@@ -14,19 +14,17 @@ export const AddBanner: React.FC<Props> = (props) => {
   const { sponsors } = props;
 
   return (
-    <div>
-      <h2 className={sponsoredStyle.sectionTitle}>ご協賛</h2>
+    <div className={`${sponsoredStyle.container}`}>
       {sponsors.map((sponsorsList, index) => (
-        <div className={sponsoredStyle.banner} key={index}>
-          <a
-            href={sponsorsList.url}
-            target="_blank"
-            rel="noreferrer"
-            key={index}
-          >
-            <img src={sponsorsList.img} />
-          </a>
-        </div>
+        <a
+          href={sponsorsList.url}
+          target="_blank"
+          rel="noreferrer"
+          key={index}
+          className={sponsoredStyle.banner}
+        >
+          <img src={sponsorsList.img} className={`${sponsoredStyle.logo}`} />
+        </a>
       ))}
     </div>
   );
