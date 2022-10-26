165df6ddc1f74af11c245cf261c6e2f2d33b0ca6
yu7400ki <yu7400ki@gmail.com>
2022-10-27 01:29:38 +0900

    delete query from TopPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,15 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { TopPage } from '../components/elements/TopPage/TopPage';
 
-const queryClient = new QueryClient();
 const Home: NextPage = () => (
-  <QueryClientProvider client={queryClient}>
+  <>
     <Head>
       <title>6th INIAD-FES</title>
     </Head>
     <TopPage />
-  </QueryClientProvider>
+  </>
 );
 
 // eslint-disable-next-line import/no-default-export
