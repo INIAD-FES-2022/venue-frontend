5e2a0bbc127df2341b38b6c850d27d8e570108fc
1F10210099 <s1f102100995@iniad.org>
2022-10-26 22:20:33 +0900

    delete flame

--- src/components/elements/About/About.css.ts
+++ src/components/elements/About/About.css.ts
@@ -1,25 +1,13 @@
 import { style } from '@vanilla-extract/css';
 
-export const container = style({
-  borderStyle: 'solid',
-  borderColor: '#811C1F',
-  borderWidth: '10px',
-  width: '100%',
-  fontSize: '1rem',
-  backgroundColor: '#FEF5EA',
-});
-
 export const siteTtl = style({
   fontSize: '2em',
-  color: '#fff',
-  backgroundColor: '#811C1F',
   width: '100%',
   padding: '30px',
   textAlign: 'left',
 });
 
 export const title = style({
-  color: '#811C1F',
   textAlign: 'left',
   fontSize: '32px',
   margin: '20px 50px 20px',
