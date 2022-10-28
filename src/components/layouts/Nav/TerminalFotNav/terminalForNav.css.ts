e73ab3444984b11a493947e92e1b00ecc72653d8
littleswampman <littleswampman@gmail.com>
2022-10-28 19:29:18 +0900

    create TerminalFotNav

---
+++ src/components/layouts/Nav/TerminalFotNav/terminalForNav.css.ts
@@ -0,0 +1,13 @@
+import { style } from '@vanilla-extract/css';
+import { terminalVars } from '../../../common/terminal/terminalTheme.css';
+
+export const styles = {
+  container: style({
+    backgroundColor: terminalVars.colors.main,
+    backdropFilter: 'blur(8px)',
+    boxShadow: '2px 4px 8px 1px rgb(1, 1, 1 , 0.72)',
+    borderRadius: '0.375rem',
+    border: `4px solid ${terminalVars.colors.terminal_frame_red}`,
+    boxSizing: 'border-box',
+  }),
+};
