7914b1c8edb70809384df381c5fe1e7b6020f759
littleswampman <littleswampman@gmail.com>
2022-10-28 16:41:16 +0900

    change url of the sponsor images

--- src/components/elements/TopPage/Sponsored/Sponsored.tsx
+++ src/components/elements/TopPage/Sponsored/Sponsored.tsx
@@ -1,6 +1,7 @@
 import { AddBanner } from './AddBanner';
 
 export const Sponsored = () => {
+  const API_ROOT = process.env.NEXT_PUBLIC_API_ROOT ?? '';
   type BannerItem = {
     // name: string; // 名前
     url: string; // URL
@@ -9,59 +10,59 @@ export const Sponsored = () => {
   const sponsorsList: BannerItem[] = [
     {
       url: 'https://www.apire.net/',
-      img: '/sponsors/アピレ.png',
+      img: `${API_ROOT}image/アピレ.png`,
     },
     // {
     //   url: 'https://goo.gl/maps/Yye178BUSuZnPNPf9',
-    //   img: '/sponsors/',
+    //   img: API_ROOT+'/sponsors/',
     // },
     {
       url: 'https://www.akabane.ed.jp/',
-      img: '/sponsors/赤羽幼稚園・赤羽こども園.png',
+      img: `${API_ROOT}image/赤羽幼稚園・赤羽こども園.png`,
     },
     {
       url: 'https://www.co-yard.com/',
-      img: '/sponsors/カレッジヤード.jpg',
+      img: `${API_ROOT}image/カレッジヤード.jpg`,
     },
     {
       url: 'https://www.maeno-yakkyoku.co.jp/shoplist/%e8%b5%a4%e7%be%bd%e6%9c%ac%e5%ba%97/',
-      img: '/sponsors/マエノ薬局.jpg',
+      img: `${API_ROOT}image/マエノ薬局.jpg`,
     },
     {
       url: 'https://www.mast-kiya.net/',
-      img: '/sponsors/シャーメゾンショップ 有限会社喜屋.png',
+      img: `${API_ROOT}image/シャーメゾンショップ 有限会社喜屋.png`,
     },
     {
       url: 'https://goo.gl/maps/WKgjX4LFbEB6uYdG7',
-      img: '/sponsors/食事処 三忠.png',
+      img: `${API_ROOT}image/食事処 三忠.png`,
     },
     {
       url: 'https://www.johokubank.jp/',
-      img: '/sponsors/城北信用金庫.jpg',
+      img: `${API_ROOT}image/城北信用金庫.jpg`,
     },
     {
       url: 'http://www.c21daikei.co.jp/',
-      img: '/sponsors/センチュリー21 大恵商事.jpg',
+      img: `${API_ROOT}image/センチュリー21 大恵商事.jpg`,
     },
     {
       url: 'https://www.next-gears.net/',
-      img: '/sponsors/ネクストギアーズ.png',
+      img: `${API_ROOT}image/ネクストギアーズ.png`,
     },
     {
       url: 'https://twitter.com/bp1_akabane',
-      img: '/sponsors/ブックスページワン イトーヨーカドー赤羽店.svg',
+      img: `${API_ROOT}image/ブックスページワン イトーヨーカドー赤羽店.svg`,
     },
     {
       url: 'https://goo.gl/maps/n5NCcZ6RuNzmxJou6',
-      img: '/sponsors/ベンディカ.jpg',
+      img: `${API_ROOT}image/ベンディカ.jpg`,
     },
     {
       url: 'http://www.machi-kita.jp/',
-      img: '/sponsors/まちづくり北株式会社.jpg',
+      img: `${API_ROOT}image/まちづくり北株式会社.jpg`,
     },
     {
       url: 'https://mangetutanuki.com/aeaefa/',
-      img: '/sponsors/手打ら〜めん満月.jpg',
+      img: `${API_ROOT}image/手打ら〜めん満月.jpg`,
     },
   ]; // 協賛のリスト
 
