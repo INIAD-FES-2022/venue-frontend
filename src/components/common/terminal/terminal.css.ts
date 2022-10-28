b0cb69fea9fa73b3745b0e1a4e62155ca5adc36c
littleswampman <littleswampman@gmail.com>
2022-10-28 14:14:15 +0900

    modify the blurring of the background of the Terminal

--- src/components/common/terminal/terminal.css.ts
+++ src/components/common/terminal/terminal.css.ts
@@ -4,7 +4,7 @@ import { terminalVars } from './terminalTheme.css';
 export const styles = {
   container: style({
     backgroundColor: terminalVars.colors.main,
-    backdropFilter: 'blur(24px)',
+    backdropFilter: 'blur(8px)',
     boxShadow: '2px 4px 8px 1px rgb(1, 1, 1 , 0.72)',
     borderRadius: '0.375rem',
     border: `4px solid ${terminalVars.colors.terminal_frame_red}`,
