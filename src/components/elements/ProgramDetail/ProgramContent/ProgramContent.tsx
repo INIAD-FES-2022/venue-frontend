a422333c3497af1af61e185c958d4af7d442cbfd
yu7400ki <yu7400ki@gmail.com>
2022-10-28 11:35:39 +0900

    fix program content component

--- src/components/elements/ProgramDetail/ProgramContent/ProgramContent.tsx
+++ src/components/elements/ProgramDetail/ProgramContent/ProgramContent.tsx
@@ -5,44 +5,43 @@ import * as common from '../common.css';
 import * as styles from './ProgramContent.css';
 
 export const ProgramContent = ({ data }: { data: Response | undefined }) => {
-  if (data) {
-    return (
-      <div className={common.container}>
-        <h2 className={common.caption}>企画へ</h2>
-        <div className={common.frame}>
-          {data.streamingUrl ? (
-            <>
-              <h3 className={common.headline3}>YouTubeLive</h3>
-              <EmbedPlayer url={data.streamingUrl} />
-            </>
-          ) : null}
-          {data.isFace2Face ? (
-            <>
-              <h3 className={common.headline3}>対面</h3>
-              <p
-                className={styles.place}
-              >{`会場は${data.place}を予定しています。`}</p>
-            </>
-          ) : null}
-          {data.relatedUrl.length > 0 ? (
-            <>
-              <h3 className={common.headline3}>関連リンク</h3>
-              <ul>
-                {data.relatedUrl.map((link, index) => (
-                  <li key={index}>
-                    <a href={link} className={common.withIcon}>
-                      <BiLink size={16} />
-                      {link}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </>
-          ) : null}
-        </div>
-      </div>
-    );
-  }
+  if (!data) return null;
+  if (!data.streamingUrl && !data.place && !data.relatedUrl.length) return null;
 
-  return null;
+  return (
+    <div className={common.container}>
+      <h2 className={common.caption}>企画へ</h2>
+      <div className={common.frame}>
+        {data.streamingUrl ? (
+          <>
+            <h3 className={common.headline3}>YouTubeLive</h3>
+            <EmbedPlayer url={data.streamingUrl} />
+          </>
+        ) : null}
+        {data.isFace2Face ? (
+          <>
+            <h3 className={common.headline3}>対面</h3>
+            <p
+              className={styles.place}
+            >{`会場は${data.place}を予定しています。`}</p>
+          </>
+        ) : null}
+        {data.relatedUrl.length > 0 ? (
+          <>
+            <h3 className={common.headline3}>関連リンク</h3>
+            <ul>
+              {data.relatedUrl.map((link, index) => (
+                <li key={index}>
+                  <a href={link} className={common.withIcon}>
+                    <BiLink size={16} />
+                    {link}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : null}
+      </div>
+    </div>
+  );
 };
