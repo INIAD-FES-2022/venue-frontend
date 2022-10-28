12128d09cb52f5b67475b9e4cd445e8ac4d3ad6f
littleswampman <littleswampman@gmail.com>
2022-10-28 10:29:26 +0900

    fix

--- src/components/common/terminal/terminal.css.ts
+++ src/components/common/terminal/terminal.css.ts
@@ -5,7 +5,7 @@ export const styles = {
   container: style({
     backgroundColor: terminalVars.colors.main,
     backdropFilter: 'blur(24px)',
-    boxShadow: '4px 8px 8px 1px rgb(1, 1, 1 , 0.72)',
+    boxShadow: '2px 4px 8px 1px rgb(1, 1, 1 , 0.72)',
     borderRadius: '0.375rem',
     border: `4px solid ${terminalVars.colors.terminal_frame_red}`,
     boxSizing: 'border-box',
