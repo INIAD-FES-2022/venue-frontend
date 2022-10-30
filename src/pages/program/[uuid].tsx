cd51c8370541665983954f46ae9402f28e9c30fa
littleswampman <littleswampman@gmail.com>
2022-10-30 10:04:10 +0900

    enable to use ISR?

--- src/pages/program/[uuid].tsx
+++ src/pages/program/[uuid].tsx
@@ -1,26 +1,46 @@
-import { useRouter } from 'next/router';
+import { GetStaticProps, GetStaticPaths } from 'next';
 import Head from 'next/head';
+import { NextPage } from 'next/types';
 import { ProgramDetail } from '../../components/elements/ProgramDetail/ProgramDetail';
 import { Layout } from '../../components/layouts/Layout';
-import type { NextPageWithLayout } from '../_app';
+import { getAllProgramIds } from '../../hooks/api/getAllProgramIds';
+import { Response } from '../../types/api/program';
+import { getProgramData } from '../../hooks/api/getProgramData';
 
-const ProgramDetailPage: NextPageWithLayout = () => {
-  const router = useRouter();
-  const { uuid } = router.query;
-
-  return (
-    <>
-      <Head>
-        <title>Program List</title>
-      </Head>
-      <ProgramDetail uuid={uuid as string} />
-    </>
-  );
+type Props = {
+  program: Response;
 };
 
-ProgramDetailPage.getLayout = (page) => (
-  <Layout title="企画詳細">{page}</Layout>
+const ProgramDetailPage: NextPage<Props> = ({ program }) => (
+  <>
+    <Head>
+      <title>Program List</title>
+    </Head>
+    <Layout title="企画詳細">
+      <ProgramDetail data={program} />
+    </Layout>
+  </>
 );
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = await getAllProgramIds();
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const program = await getProgramData(ctx?.params?.uuid as string);
+
+  return {
+    props: {
+      program,
+    },
+    revalidate: 3,
+  };
+};
 
 // eslint-disable-next-line import/no-default-export
 export default ProgramDetailPage;
