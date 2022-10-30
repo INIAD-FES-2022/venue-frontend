fb3437ffd5a38e6789e8515c8e50c17f71220703
littleswampman <littleswampman@gmail.com>
2022-10-30 10:00:23 +0900

    NextPageWithLayout is no longer used in _app.tsx

--- src/pages/notice.tsx
+++ src/pages/notice.tsx
@@ -1,18 +1,18 @@
 import Head from 'next/head';
+import { NextPage } from 'next/types';
 import { NoticeList } from '../components/elements/NoticeList/NoticeList';
 import { Layout } from '../components/layouts/Layout';
-import type { NextPageWithLayout } from './_app';
 
-const NoticePage: NextPageWithLayout = () => (
+const NoticePage: NextPage = () => (
   <>
     <Head>
       <title>Notice List</title>
     </Head>
-    <NoticeList />
+    <Layout title="問い合わせ">
+      <NoticeList />
+    </Layout>
   </>
 );
-
-NoticePage.getLayout = (page) => <Layout title="問い合わせ">{page}</Layout>;
 
 // eslint-disable-next-line import/no-default-export
 export default NoticePage;
--- src/pages/program/index.tsx
+++ src/pages/program/index.tsx
@@ -1,18 +1,18 @@
 import Head from 'next/head';
+import { NextPage } from 'next/types';
 import { ProgramList } from '../../components/elements/ProgramList/ProgramList';
 import { Layout } from '../../components/layouts/Layout';
-import type { NextPageWithLayout } from '../_app';
 
-const ProgramPage: NextPageWithLayout = () => (
+const ProgramPage: NextPage = () => (
   <>
     <Head>
       <title>Program List</title>
     </Head>
-    <ProgramList />
+    <Layout title="企画一覧">
+      <ProgramList />
+    </Layout>
   </>
 );
-
-ProgramPage.getLayout = (page) => <Layout title="企画一覧">{page}</Layout>;
 
 // eslint-disable-next-line import/no-default-export
 export default ProgramPage;
