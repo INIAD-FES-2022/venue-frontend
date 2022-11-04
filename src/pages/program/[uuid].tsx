baa1a7cc964efb63b6e7a79a36ca2ed9e68daaf1
littleswampman <littleswampman@gmail.com>
2022-11-04 12:14:17 +0900

    fix to do not use SSG

--- src/pages/program/[uuid].tsx
+++ src/pages/program/[uuid].tsx
@@ -1,45 +1,23 @@
-import { GetStaticProps, GetStaticPaths } from 'next';
 import Head from 'next/head';
 import { NextPage } from 'next/types';
+import { useRouter } from 'next/router';
 import { ProgramDetail } from '../../components/elements/ProgramDetail/ProgramDetail';
 import { Layout } from '../../components/layouts/Layout';
-import { getAllProgramIds } from '../../hooks/api/getAllProgramIds';
-import { Response } from '../../types/api/program';
-import { getProgramData } from '../../hooks/api/getProgramData';
 
-type Props = {
-  program: Response;
-};
+const ProgramDetailPage: NextPage = () => {
+  const router = useRouter();
+  const { uuid } = router.query;
 
-const ProgramDetailPage: NextPage<Props> = ({ program }) => (
-  <>
-    <Head>
-      <title>Program List</title>
-    </Head>
-    <Layout title="企画詳細">
-      <ProgramDetail data={program} />
-    </Layout>
-  </>
-);
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = await getAllProgramIds();
-
-  return {
-    paths,
-    fallback: true,
-  };
-};
-
-export const getStaticProps: GetStaticProps = async (ctx) => {
-  const program = await getProgramData(ctx?.params?.uuid as string);
-
-  return {
-    props: {
-      program,
-    },
-    revalidate: 3,
-  };
+  return (
+    <>
+      <Head>
+        <title>Program List</title>
+      </Head>
+      <Layout title="企画詳細">
+        <ProgramDetail uuid={uuid as string} />
+      </Layout>
+    </>
+  );
 };
 
 // eslint-disable-next-line import/no-default-export
